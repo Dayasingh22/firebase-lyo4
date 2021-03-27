89d66824d42a4d6a6df79d55c2e8dd31e6295223
Dayasingh22 <dayanidhi.singh.sd@mmumullana.org>
2021-03-26 20:15:10 -0700

    Set up firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,20 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
-
-
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
 var config = {
-  apiKey: "AIzaSyDmWlaEkgA2o7A4qkVeEXaC2N_OWFEaMd0",
-  authDomain: "lyoimsweb.firebaseapp.com",
-  projectId: "lyoimsweb",
-  storageBucket: "lyoimsweb.appspot.com",
-  messagingSenderId: "407922520085",
-  appId: "1:407922520085:web:8a80058bc9d0d32c209e1c"
-}
+  apiKey: "AIzaSyAopLfxxiFAwmrxBddBXLfCjxJyvuDkfWU",
+  authDomain: "lyoimsweb-b0810.firebaseapp.com",
+  projectId: "lyoimsweb-b0810",
+  storageBucket: "lyoimsweb-b0810.appspot.com",
+  messagingSenderId: "1017799830672",
+  appId: "1:1017799830672:web:78a81ee7d051508c563c15",
+};
 
-const app = firebase.initializeApp(config)
-export const auth = app.auth()
-export const storage = app.storage();//storage
-export const storageRef = storage.ref()
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const storage = app.storage(); //storage
+export const storageRef = storage.ref();
 export const db = app.firestore();
-export default app
+export default app;
